9863e0817d119110628147c20791eeaed9daba4f
Giovanni Lodi <giovanni.lodi42@gmail.com>
2015-03-16 06:47:29 +1100

    Initial Commit

---
+++ specta-global-before-after-each/Controllers/AppDelegate.swift
@@ -0,0 +1,14 @@
+//
+//  AppDelegate.swift
+//  specta-global-before-after-each
+//
+//  Created by Giovanni Lodi on 3/16/15
+//  Copyright (c) 2015 mokacoding. All rights reserved.
+//
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+    var window: UIWindow?
+}
